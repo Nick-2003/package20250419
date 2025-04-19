5ade80915777f6f0b001d0ad2ad2ddd24d9d1489
Nick-2003 <tsunlinicholastam@gmail.com>
2025-04-19 12:58:50 -0700

    ntam-package_process-m4

--- R/temp_conv.R
+++ R/temp_conv.R
@@ -1,23 +1,54 @@
-#' Check if a year is a leap year.
+#' Convert temperature values
 #' 
-#' @param year Year to input
+#' @param temp Temperature in given start_scale
+#' @param start_scale Temperature scale for start; "F" for Farenheit, "C" for Celcius, "K" for Kelvin
+#' @param end_scale Temperature scale for end; "F" for Farenheit, "C" for Celcius, "K" for Kelvin
 #' 
-#' @return Boolean
+#' @return Temperature in given end_scale
 #' 
 #' @export
 #' 
 #' @examples
-#' is_leap(2000)
-#' #> TRUE
-#' is_leap(1900)
-#' #> FALSE
+#' temp_conv(41, "F", "C")
+#' #> 5
+#' temp_conv(5, "C", "K")
+#' #> 278.15
+#' temp_conv(278.15, "K", "F")
+#' #> 41
 #' 
-is_leap <- function(year) {
-  if (year <= 0 || year %% 1 != 0) {
-    return(stop()) # Throw error if year is 0 or less
-  } else if (year %% 4 == 0 && year %% 100 != 0 || year %% 400 == 0) {
-    return(TRUE)
-  } else {
-    return(FALSE)
+temp_conv <- function(temp, start_scale, end_scale) {
+  if (!(start_scale %in% c("F", "C", "K")) ||
+        !(end_scale %in% c("F", "C", "K"))) {
+    stop("Invalid start or end scale.")
+  } else if (!is.numeric(temp)) {
+    stop("Invalid temperature value.")
+  } else if ((temp < 0 && start_scale == "K") || (temp < -273.15 && start_scale == "C") || (temp < -459.67 && start_scale == "F")) {
+    stop("Invalid temperature.")
   }
+
+  if (start_scale == end_scale) {
+    return(temp)
+  }
+
+  if (start_scale == "F") {
+    if (end_scale == "C") {
+      result <- (temp - 32) * (5 / 9)
+    } else if (end_scale == "K") {
+      result <- ((temp - 32) * (5 / 9)) + 273.15
+    }
+  } else if (start_scale == "C") {
+    if (end_scale == "F") {
+      result <- (temp * (9 / 5)) + 32
+    } else if (end_scale == "K") {
+      result <- temp + 273.15
+    }
+  } else if (start_scale == "K") {
+    if (end_scale == "F") {
+      result <- ((temp - 273.15) * (9 / 5)) + 32
+    } else if (end_scale == "C") {
+      result <- temp - 273.15
+    }
+  }
+
+  return(result)
 }
